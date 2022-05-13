d85409586ab197682bc75c68e09493b08ce6bfc6
Jhonn Rodriguez <jhonn@wehaa.com>
2022-05-13 13:29:56 -0400

    Improve btn and checkbox

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
          <ul class="todo-list mt-4">
           <li v-for="item in items" :key="item.id" class="flex justify-between items-center mt-3" >
             <div class="flex items-center" :class="{'line-through': item.completed}">
-              <input class="" :class="{'text-black': item.completed}" type="checkbox" v-model="item.completed" @click="completeTask(item)" >
+              <input class="checkbox h-5 w-5" :class="{'text-black': item.completed}" type="checkbox" v-model="item.completed" @click="completeTask(item)" >
               <div class="capitalize ml-3 text-sm font-normal">{{ item.text }}</div>
             </div>
             <div>
@@ -16,8 +16,10 @@
         </ul>
         <div class="w-full flex relative items-center">
           <input @keyup.enter="addTask" class="mt-3 px-2 w-full bg-gray-200 focus:bg-white" type="text" v-model="newItem" placeholder="Start typing and hit enter to create a new ToDo">
-          <button class="absolute border border-black  right-1 rounded rounded-full px-1 py-0 h-6 bottom-0" @click="addTask">
-           +
+          <button class="absolute right-1 bottom-0" @click="addTask">
+           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 opacity-60" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
           </button>
         </div>
 
@@ -54,3 +56,34 @@ export default {
   }
 }
 </script>
+<style scoped>
+ .checkbox {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    -webkit-print-color-adjust: exact;
+    color-adjust: exact;
+    display: inline-block;
+    vertical-align: middle;
+    background-origin: border-box;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    flex-shrink: 0;
+    height: 32px;
+    width: 32px;
+    color: #000;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    border-radius: 8px;
+}
+.checkbox:checked {
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
+    border-color: transparent;
+    background-color: currentColor;
+    background-size: 100% 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
